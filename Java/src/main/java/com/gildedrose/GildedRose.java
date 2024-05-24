216f1c0033c14c1ef11189e5bbeeb2824fb4fc08
RobbeHeirman <robbeheirman@msn.com>
2024-05-24 09:51:58 +0200

    25: part1 refactor backstage logic. Move sellIn logic on top to be consistent

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -20,26 +20,27 @@ class GildedRose {
     }
 
     private static void updateBackStageItem(final Item item) {
+        item.sellIn--;
+
+        if (item.sellIn < 0) {
+            item.quality = 0;
+            return;
+        }
+
         if (item.quality < 50) {
             item.quality = item.quality + 1;
         }
-        if (item.sellIn < 11) {
+        if (item.sellIn < 10) {
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
             }
         }
 
-        if (item.sellIn < 6) {
+        if (item.sellIn < 5) {
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
             }
         }
-        item.sellIn--;
-
-        if (item.sellIn >= 0) {
-            return;
-        }
-        item.quality = item.quality - item.quality;
     }
 
     public void updateQuality() {
