bbdecf76897f739fc602279ebe6c1b86a5ba5a96
RobbeHeirman <robbeheirman@msn.com>
2024-05-24 09:31:44 +0200

    18: Move backstage pass logic in backstage pass branch

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -21,17 +21,7 @@ class GildedRose {
                     item.quality = item.quality + 1;
 
                     if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
 
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
                     }
                 }
             }
@@ -47,6 +37,17 @@ class GildedRose {
                     item.quality = item.quality + 1;
                 }
             } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.sellIn < 11) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                }
+
+                if (item.sellIn < 6) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                }
                 item.sellIn--;
 
                 if (item.sellIn >= 0) {
