8cef6109a3085bb30ec17cccc711633b4f78e9de
RobbeHeirman <robbeheirman@msn.com>
2024-05-26 17:50:23 +0200

    62: Document RunnableFactory

--- Java/src/main/java/com/gildedrose/item_runnables/ItemRunnableFactory.java
+++ Java/src/main/java/com/gildedrose/item_runnables/ItemRunnableFactory.java
@@ -5,9 +5,16 @@ import com.gildedrose.item_constraints.ItemConstraint;
 import com.gildedrose.item_update_strategy.StrategySelector;
 import com.gildedrose.item_update_strategy.UpdateStrategy;
 
+/**
+ * Interface for ItemRunnableFactories. A factory will take an {@link Item} and return a {@link Runnable}.
+ */
 public interface ItemRunnableFactory {
     Runnable getRunnable(Item item);
 
+    /**
+     * Will return a runnable for the item corresponding to the gilded rose specs.
+     * @see <a href="https://github.com/emilybache/GildedRose-Refactoring-Kata/blob/main/GildedRoseRequirements.md">Specs</a>
+     */
     static Runnable defaultRunnableFactory(Item item) {
         ItemConstraint constraint = ItemConstraint.getItemConstraints(item);
         UpdateStrategy strategy = StrategySelector.defaultStrategySelector(item);
