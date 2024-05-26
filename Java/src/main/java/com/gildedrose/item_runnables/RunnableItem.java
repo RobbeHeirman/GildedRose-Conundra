a49a456a8aafdba47da228ef7ff41a735bffe262
RobbeHeirman <robbeheirman@msn.com>
2024-05-26 17:52:41 +0200

    63: Document RunnableItem

--- Java/src/main/java/com/gildedrose/item_runnables/RunnableItem.java
+++ Java/src/main/java/com/gildedrose/item_runnables/RunnableItem.java
@@ -6,6 +6,12 @@ import com.gildedrose.item_update_strategy.UpdateStrategy;
 
 import java.util.Arrays;
 
+/**
+ * Wrapper class for creating a runnable item. This can be done with functions but if we need to have some
+ * sort of state for RunnableItems later a class could be more convenient.
+ * Will take an item, a strategy and some constraints on the item. And apply the strategy to the item if
+ * the run method is called.
+ */
 public class RunnableItem implements Runnable {
     private final Item item;
     private final UpdateStrategy strategy;
